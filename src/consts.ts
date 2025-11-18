81e5abf79c9da5f1a412d5d3afd25f6bfee8edcd
hermetrixtech <cy.hermetrix@protonmail.com>
2025-11-18 23:42:37 +1300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Hermetrix Technologies";
+export const SITE_DESCRIPTION = "Secure Communications & Cybersecurity Solutions for the Asia-Pacific Region";
+export const SITE_URL = "https://hermetrix.nz";
